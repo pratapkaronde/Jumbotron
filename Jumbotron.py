2bbd006f590bc358a31bc5f7c28efa9824a95fe1
Pratap Karonde <pratap.karonde@gmail.com>
2020-07-25 16:29:10 -0500

    Smoot Scrolling Text

--- Jumbotron.py
+++ Jumbotron.py
@@ -2,11 +2,11 @@ import pygame
 from pygame.locals import KEYDOWN, K_ESCAPE
 from bitmap import font_bitmap, CHARACTER_WIDTH, CHARACTER_HEIGHT
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
 LED_RADIUS = 5
 LED_DIAMETER = LED_RADIUS * 2
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = LED_DIAMETER * CHARACTER_HEIGHT  # 600
 
 COLOR_BACKGROUND = (0, 0, 128)
 COLOR_LED_ON = (200, 200, 0)
@@ -16,6 +16,9 @@ COLOR_LED_OFF = (150, 50, 0)
 def displayMessage(message, start_x=0):
 
     character_number = 0
+    x_increment = LED_DIAMETER + 2
+    x_start_character = LED_RADIUS
+    x_position = x_start_character
 
     for letter in message:
 
@@ -24,23 +27,26 @@ def displayMessage(message, start_x=0):
         for y in range(CHARACTER_HEIGHT):
 
             mask = int(128)
-            byte = font_bitmap[(ascii - 32)][13 - y - 1]
+            byte = font_bitmap[(ascii - 32)][CHARACTER_HEIGHT - y - 1]
 
-            for x in range(CHARACTER_WIDTH):
+            x_position = x_start_character
+            for x in range(start_x, CHARACTER_WIDTH):
 
-                x_offset = ((x * LED_DIAMETER) + 2) + (character_number * \
-                            (CHARACTER_WIDTH * (LED_DIAMETER))) + LED_RADIUS
+                # x_position = ((x * LED_DIAMETER) + 2) + (character_number *
+                #                                         (CHARACTER_WIDTH * (LED_DIAMETER))) + LED_RADIUS
 
-                y_offset = ((y * LED_DIAMETER) + 2) + LED_RADIUS
+                y_position = ((y * LED_DIAMETER) + 2) + LED_RADIUS
 
                 led_color = COLOR_LED_OFF
                 if (int(byte) & int(mask)):
                     led_color = COLOR_LED_ON
 
-                pygame.draw.circle(screen, led_color, (x_offset, y_offset), LED_RADIUS)
+                pygame.draw.circle(screen, led_color, (x_position, y_position), LED_RADIUS)
 
+                x_position = x_position + x_increment
                 mask = mask / 2
 
+        x_start_character = x_position
         start_x = 0
         character_number = character_number + 1
 
@@ -56,7 +62,7 @@ if __name__ == "__main__":
     running = True
     screen.fill(COLOR_BACKGROUND)
 
-    message = "Hello,World! "
+    message = "Pratap Karonde "
     timer = pygame.time.Clock()
 
     x = 0
@@ -80,4 +86,4 @@ if __name__ == "__main__":
 
         x = x + 1
 
-        timer.tick(6)
+        timer.tick(15)
